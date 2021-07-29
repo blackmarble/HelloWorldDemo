4dc4f81ef022c8ef3dc1301b23ea62bb24be3f16
Richard Fennell <richard@blackmarble.co.uk>
2021-07-29 14:14:20 +0100

    Initial commit

---
+++ src/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace HelloWorld
+{
+    public class WebSite
+    {
+
+        public static void Main(string[] args){
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://0.0.0.0:5000")
+                .Configure(app => app.Run(async context => {
+                    await context.Response.WriteAsync("Hello World!");
+                }))
+                .Build();
+
+            host.Run();
+        }
+    }
+}
